49b48547c96cc1ad61026f0e68123f6fe38bca51
faiz <faizahmad.daiee@gmail.com>
2023-02-22 05:07:10 -0800

    remove tag line form header

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -14,20 +15,18 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
     <!-- link bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <!-- link assets/style.css -->
-    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;700&display=swap" rel="stylesheet">
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    
+
     <!-- <script src="assets/script.js"></script> -->
 
     <?php wp_head(); ?>
@@ -37,32 +36,23 @@
     <?php wp_body_open(); ?>
 
     <!-- cover background -->
-    <div class="cover-bg"
-        style="background: linear-gradient(rgba(19, 34, 66, 0.85), rgba(19, 34, 66, 0.85)),
-		<?if ( is_front_page() && is_home() ) {
-			echo 'url('.get_header_image().');';
-			}?>">
+    <div class="cover-bg" style="background: linear-gradient(rgba(19, 34, 66, 0.85), rgba(19, 34, 66, 0.85)),
+		<? if (is_front_page() && is_home()) {
+            echo 'url(' . get_header_image() . ');';
+        } ?>">
         <!-- top header -->
         <div class="container top-header">
             <div class="row">
                 <div class="col">
                     <!-- logo -->
-                    <?php
-						echo get_custom_logo();
-						
-						$ehsanlawpllc_description = get_bloginfo( 'description', 'display' );
-						if ( $ehsanlawpllc_description || is_customize_preview() ) : ?>
-                    <p class="site-description">
-                        <?php echo $ehsanlawpllc_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-                    </p>
-                    <?php endif; ?>
+                    <?php echo get_custom_logo(); ?>
 
                 </div>
                 <div class="col d-flex align-items-center">
 
                     <div class="ms-auto">
-						<?php dynamic_sidebar( 'header-social-media-icons' ); ; ?>
-						
+                        <?php dynamic_sidebar('header-social-media-icons');; ?>
+
                         <!-- <ul class="social-top">
                             <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"
                                         viewBox="0 0 24 24">
@@ -96,24 +86,22 @@
         <nav id="menu" class="navbar navbar-expand-lg bg-dark p-0 sticky-top">
             <div class="container-fluid">
 
-                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="navbar-collapse text-center collapse" id="navbarSupportedContent" style="">
 
                     <?php
-					wp_nav_menu(array(
-						'theme_location' => 'main-menu',
-						'container' => false,
-						'menu_class' => '',
-						'fallback_cb' => '__return_false',
-						'items_wrap' => '<ul id="%1$s" class="navbar-nav m-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
-						'depth' => 2,
-						'walker' => new bootstrap_5_wp_nav_menu_walker()
-					));
-					?>
+                    wp_nav_menu(array(
+                        'theme_location' => 'main-menu',
+                        'container' => false,
+                        'menu_class' => '',
+                        'fallback_cb' => '__return_false',
+                        'items_wrap' => '<ul id="%1$s" class="navbar-nav m-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                        'depth' => 2,
+                        'walker' => new bootstrap_5_wp_nav_menu_walker()
+                    ));
+                    ?>
 
                 </div>
             </div>
@@ -122,12 +110,11 @@
 
         <!-- feutured -->
         <!-- if home page -->
-        <?php 
-		if ( is_front_page() && is_home() ) {
-			dynamic_sidebar( 'home-feutured' );
-			
-			}?>
-		<!-- /feutured -->
+        <?php
+        if (is_front_page() && is_home()) {
+            dynamic_sidebar('home-feutured');
+        } ?>
+        <!-- /feutured -->
 
     </div>
     <!-- /cover background -->
